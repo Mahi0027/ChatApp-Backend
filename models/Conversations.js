db5ee825b2a559a16cb80af69e169b38e8c1ccd1
Mahipal Singh <50288121+Mahi0027@users.noreply.github.com>
2023-12-09 01:40:37 +0530

    group chat feature added

--- models/Conversations.js
+++ models/Conversations.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
 const ConversationSchema = mongoose.Schema({
+    groupName: {
+        type: String,
+    },
     members: {
         type: Array,
         required: true,
-    }
+    },
+    isGroup: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 const Conversation = mongoose.model("Conversation", ConversationSchema);
